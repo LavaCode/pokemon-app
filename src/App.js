ea31cc06d462fb07e4ac005f34653b4010b95ba8
LavaCode <kasper.zegel@gmail.com>
2021-04-01 16:32:26 +0200

    Added basic styling, sprites and pokemons

--- src/App.js
+++ src/App.js
@@ -1,51 +1,46 @@
-import React, { useState, useEffect  } from 'react';
-import Buttons from './components/PageButton';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import Pokemons from './components/Pokemons';
 import './App.css';
 
 const debugMode = false;
-debugMode && console.log('-- Debug mode is ACTIVATED --');
+debugMode && console.log('-- Debug App is ACTIVATED --');
 
 function App() {
   const [pokemons, setPokemons] = useState([])
   const [error, setError] = useState('');
-  const [loading, toggleLoading] = useState(false);
-
-  async function fetchData() {
-    const result = await axios.get('https://pokeapi.co/api/v2/pokemon/');
-    debugMode && console.log(result.data)
-    setPokemons(result.data.results)
-    debugMode && console.log(pokemons)
-  }
-
-  useEffect(() => {
-    setError('');
-    toggleLoading(true);
-
-    try { 
-      fetchData();
-    } 
-    catch(e) {
-      console.error(e);
-      setError(e.message);
-    } 
-    toggleLoading(false);
-  }, [])
+  const [loading, toggleLoading] = useState(false); 
+  
+    useEffect(() => {
+      setError('');
+      toggleLoading(true);
+      try { 
+        fetchData();
+      } 
+      catch(e) {
+        console.error(e);
+        setError(e.message);
+      } 
+      toggleLoading(false);
+    }, [])
+  
+    async function fetchData() {
+      const result = await axios.get(`https://pokeapi.co/api/v2/pokemon`);
+      debugMode && console.log(result.data);
+      setPokemons(result.data.results);
+      debugMode && console.log(pokemons);
+    }
 
   return (
     <>
-    <div>
-      {loading && (<p>Data wordt opgehaald...</p>)}
-      <Buttons name="vorige"/>
-      <Buttons name="volgende"/>
-      <ul>
-          {pokemons && pokemons.map((pokemon) => (
-          <li key={pokemon.name}>{pokemon.name}</li>
-        ))}
-      </ul>
-    </div>
-
-    {error && (<span>Ho! er gaat iets fout hier </span>)}
+      <div>
+            {pokemons && pokemons.map((pokemon) => {
+              <p>{pokemon.name}</p>
+              return (
+                <Pokemons name={pokemon.name}/>
+              )
+              })}
+      </div>
     </>
   );
 }
